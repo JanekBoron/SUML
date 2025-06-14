0cfbb745937aa716d403c838cd4add65d66254dc
Jan Boroń <jan.boron2001@gmail.com>
2025-06-14 03:20:13 +0200

    SUML 6/poprawki CD

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -35,3 +39,4 @@ resource "azurerm_linux_web_app" "app" {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
 }
+
--- infra/variables.tf
+++ infra/variables.tf
@@ -19,3 +19,20 @@ variable "docker_image" {
   type        = string
   default     = "boronekziomek/asystent-snu:latest"  
 }
+
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+  sensitive   = true
+}
