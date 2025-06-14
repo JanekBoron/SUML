6fe9f8716fee393373c48a2ed556f676fc9bd14a
Jan Boroń <jan.boron2001@gmail.com>
2025-06-14 03:12:25 +0200

    SUML 6/poprawki_pipeline CD

--- infra/main.tf
+++ infra/main.tf
@@ -1,53 +1,48 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-rg"
-  location = var.location
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = lower("${var.prefix}sa")
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "logs" {
-  name                  = "logs"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
+  name     = "suml-rg"
+  location = "East US"
 }
 
 resource "azurerm_app_service_plan" "plan" {
-  name                = "${var.prefix}-plan"
+  name                = "suml-appserviceplan"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  sku_name            = "B1"
+  kind                = "Linux"
+  reserved            = true
 }
 
 resource "azurerm_web_app" "app" {
-  name                = "${var.prefix}-webapp"
+  name                = "suml-webapp"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
   app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
-    linux_fx_version = "DOCKER|${var.container_registry}/${var.image_name}:latest"
+    linux_fx_version = "DOCKER|${var.docker_image}"
   }
 
-  identity {
-    type = "SystemAssigned"
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
 }
 
-resource "azurerm_role_assignment" "blob_contributor" {
-  scope                = azurerm_storage_account.sa.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_web_app.app.identity.principal_id
+variable "docker_image" {
+  type    = string
+  default = "yourdockeruser/asystent-snu:latest"
 }
