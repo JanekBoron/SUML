f787769c57d2de09c99fa9d06381ec07c3642b26
Jan Boroń <jan.boron2001@gmail.com>
2025-06-14 03:14:23 +0200

    SUML 6/dzialaj prooosze

--- infra/main.tf
+++ infra/main.tf
@@ -1,37 +1,31 @@
-terraform {
-  required_version = ">= 1.1.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "suml-rg"
-  location = "East US"
+  location = "westeurope"
 }
 
 resource "azurerm_app_service_plan" "plan" {
   name                = "suml-appserviceplan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "B1"
   kind                = "Linux"
-  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+  reserved = true
 }
 
-resource "azurerm_web_app" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = "suml-webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  service_plan_id     = azurerm_app_service_plan.plan.id
 
   site_config {
     linux_fx_version = "DOCKER|${var.docker_image}"
@@ -40,9 +34,4 @@ resource "azurerm_web_app" "app" {
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
-}
-
-variable "docker_image" {
-  type    = string
-  default = "yourdockeruser/asystent-snu:latest"
 }
